e0466831bc05a2b58613363a87c462ec84c6ce60
Caio4breu <abreuocaio@gmail.com>
2026-06-02 16:44:06 -0300

    feat(config): adiciona models e DTOs da camada de domínio

---
+++ src/main/java/br/com/picarauto/model/Pecamodel.java
@@ -0,0 +1,19 @@
+package br.com.picarauto.model;
+
+import java.math.BigDecimal;
+
+public class PecaModel extends BaseModel {
+
+    private String nome;
+    private Integer quantidade;
+    private BigDecimal valorUnitario;
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public Integer getQuantidade() { return quantidade; }
+    public void setQuantidade(Integer quantidade) { this.quantidade = quantidade; }
+
+    public BigDecimal getValorUnitario() { return valorUnitario; }
+    public void setValorUnitario(BigDecimal valorUnitario) { this.valorUnitario = valorUnitario; }
+}
