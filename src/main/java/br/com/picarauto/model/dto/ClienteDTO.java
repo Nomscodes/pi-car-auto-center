dd1dbd1fdbf4e2e7dfb441e2ae05265088d3339e
Caio4breu <abreuocaio@gmail.com>
2026-06-03 17:59:44 -0300

    refactor(cliente): adapta Repository, Mapper, DTO e Validation para nova hierarquia de Pessoa

--- src/main/java/br/com/picarauto/model/dto/ClienteDTO.java
+++ src/main/java/br/com/picarauto/model/dto/ClienteDTO.java
@@ -1,32 +1,27 @@
 package br.com.picarauto.model.dto;
 
-public class ClienteDTO extends BaseDTO {
-    private String nome;
-    private String cpf;
-    private String email;
-    private String telefone;
-    private String endereco;
-    private String bairro;
-    private String cidade;
-    private String estado;
-    private String cep;
+import java.util.Date;
 
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
-    public String getCpf() { return cpf; }
-    public void setCpf(String cpf) { this.cpf = cpf; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+public class ClienteDTO extends BaseDTO {
+
+    private String nomeCompleto;
+    private String telefone;
+    private String email;
+    private String endereco;
+    private Date dataCadastro;
+
+    public String getNomeCompleto() { return nomeCompleto; }
+    public void setNomeCompleto(String nomeCompleto) { this.nomeCompleto = nomeCompleto; }
+
     public String getTelefone() { return telefone; }
     public void setTelefone(String telefone) { this.telefone = telefone; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
     public String getEndereco() { return endereco; }
     public void setEndereco(String endereco) { this.endereco = endereco; }
-    public String getBairro() { return bairro; }
-    public void setBairro(String bairro) { this.bairro = bairro; }
-    public String getCidade() { return cidade; }
-    public void setCidade(String cidade) { this.cidade = cidade; }
-    public String getEstado() { return estado; }
-    public void setEstado(String estado) { this.estado = estado; }
-    public String getCep() { return cep; }
-    public void setCep(String cep) { this.cep = cep; }
+
+    public Date getDataCadastro() { return dataCadastro; }
+    public void setDataCadastro(Date dataCadastro) { this.dataCadastro = dataCadastro; }
 }
