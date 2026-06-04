1cf0c9c71b40e3c711d9c91e88eae4180d171884
Gabriel Naoki <gabrielnaokiu@gmail.com>
2026-06-04 17:55:34 -0300

    feat(colaborador): adiciona ServicoDoColaboradorModel e ServicoDoColaboradorDTO

---
+++ src/main/java/br/com/picarauto/model/dto/ServicoDoColaboradorDTO.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.picarauto.model.dto;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class ServicoDoColaboradorDTO extends BaseDTO {
+
+    private Integer idColaborador;
+    private Integer idServicoInterno;
+    private LocalDate dataServico;
+
+    public Integer getIdColaborador() {
+        return idColaborador;
+    }
+
+    public void setIdColaborador(Integer idColaborador) {
+        this.idColaborador = idColaborador;
+    }
+
+    public Integer getIdServicoInterno() {
+        return idServicoInterno;
+    }
+
+    public void setIdServicoInterno(Integer idServicoInterno) {
+        this.idServicoInterno = idServicoInterno;
+    }
+
+    public LocalDate getDataServico() {
+        return dataServico;
+    }
+
+    public void setDataServico(LocalDate dataServico) {
+        this.dataServico = dataServico;
+    }
+}
