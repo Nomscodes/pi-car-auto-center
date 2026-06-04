0194fd557f3e837ddc24bf2ce845469136ca53c3
Caio4breu <abreuocaio@gmail.com>
2026-06-03 23:13:47 -0300

    style(cliente): adiciona assinatura @author Caio4breu

--- src/main/java/br/com/picarauto/repository/IPessoaJuridicaRepository.java
+++ src/main/java/br/com/picarauto/repository/IPessoaJuridicaRepository.java
@@ -1,5 +1,9 @@
 package br.com.picarauto.repository;
 
+/**
+ *
+ * @author Caio4breu
+ */
 import br.com.picarauto.model.PessoaJuridicaModel;
 
 public interface IPessoaJuridicaRepository extends IGenericRepository<PessoaJuridicaModel> {
