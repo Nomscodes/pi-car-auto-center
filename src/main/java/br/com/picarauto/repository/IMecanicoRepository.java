2c0e54fd5cdc8fd47575f146bc6c2ea6dcb7237a
Caio4breu <abreuocaio@gmail.com>
2026-06-05 03:50:33 -0300

    refactor(colaborador): remove camada Mecanico substituída por ColaboradorModel

--- src/main/java/br/com/picarauto/repository/IMecanicoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.picarauto.repository;
-
-import br.com.picarauto.model.MecanicoModel;
-
-public interface IMecanicoRepository extends IGenericRepository<MecanicoModel> {
-    boolean existsByCpf(String cpf);
-}
