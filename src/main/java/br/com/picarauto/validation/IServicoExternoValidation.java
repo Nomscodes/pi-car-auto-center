7092ec3aef78d36a981620ce41e0c64c5e8788e0
Caio4breu <abreuocaio@gmail.com>
2026-06-05 19:40:32 -0300

    feat(os): adiciona IServicoExternoValidation e ServicoExternoValidation

---
+++ src/main/java/br/com/picarauto/validation/IServicoExternoValidation.java
@@ -0,0 +1,10 @@
+package br.com.picarauto.validation;
+
+import br.com.picarauto.model.ServicoExternoModel;
+import br.com.picarauto.repository.IServicoExternoRepository;
+
+/**
+ *
+ * @author Caio4breu
+ */
+public interface IServicoExternoValidation extends IGenericValidation<ServicoExternoModel, IServicoExternoRepository> {}
