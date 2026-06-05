0c60d77d2422726385fdf3b35300d7703fe99bd0
Caio4breu <abreuocaio@gmail.com>
2026-06-05 02:48:16 -0300

    refactor(os): corrige factory e decorators para ItemServicoInternoModel

--- src/main/java/br/com/picarauto/model/ItemServicoInternoModel.java
+++ src/main/java/br/com/picarauto/model/ItemServicoInternoModel.java
@@ -1,24 +1,32 @@
 package br.com.picarauto.model;
 
 /**
+ * Representa a execução de um serviço interno vinculado a uma OS.
+ *
+ * Implementa {@link IItemServicoOS} para que a factory de serviços internos
+ * possa criá-lo polimorficamente via {@link br.com.picarauto.factory.ServicoInternoFactory}.
  *
  * @author Caio4breu
  */
-public class ItemServicoInternoModel extends BaseModel {
+public class ItemServicoInternoModel extends BaseModel implements IItemServicoOS {
+
     private double valorItem;
     private int garantia;
     private String observacoes;
     private Integer idOS;
- 
+
+    @Override
+    public Integer getId() { return super.getId(); }
+
+    @Override
+    public String getDescricao() { return observacoes; }
+
     public double getValorItem() { return valorItem; }
     public void setValorItem(double valorItem) { this.valorItem = valorItem; }
- 
     public int getGarantia() { return garantia; }
     public void setGarantia(int garantia) { this.garantia = garantia; }
- 
     public String getObservacoes() { return observacoes; }
     public void setObservacoes(String observacoes) { this.observacoes = observacoes; }
- 
     public Integer getIdOS() { return idOS; }
-    public void setIdOS(Integer idOS) { this.idOS = idOS; }    
+    public void setIdOS(Integer idOS) { this.idOS = idOS; }
 }
