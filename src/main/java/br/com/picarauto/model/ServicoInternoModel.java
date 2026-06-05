cefb7b4056842e86ec64ba1fca3d6c245a5ff378
Caio4breu <abreuocaio@gmail.com>
2026-06-05 02:48:42 -0300

    feat(os): adiciona cadeia completa de ServicoInterno

---
+++ src/main/java/br/com/picarauto/model/ServicoInternoModel.java
@@ -0,0 +1,17 @@
+package br.com.picarauto.model;
+
+/**
+ *
+ * @author Caio4breu
+ */
+public class ServicoInternoModel extends BaseModel {
+
+    private String descricao;
+    private double valorCobrado;
+
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+
+    public double getValorCobrado() { return valorCobrado; }
+    public void setValorCobrado(double valorCobrado) { this.valorCobrado = valorCobrado; }
+}
