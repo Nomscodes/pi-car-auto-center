f7e20a1ecdde769ecbce681c9ee4f9f34f7a7489
Caio4breu <abreuocaio@gmail.com>
2026-06-05 02:47:38 -0300

    refactor(os): corrige model, repository, service e validation para o schema real

--- src/main/java/br/com/picarauto/repository/IOrdemServicoRepository.java
+++ src/main/java/br/com/picarauto/repository/IOrdemServicoRepository.java
@@ -1,7 +1,13 @@
 package br.com.picarauto.repository;
 
 import br.com.picarauto.model.OrdemServicoModel;
+import java.util.List;
 
+/**
+ *
+ * @author Caio4breu
+ */
 public interface IOrdemServicoRepository extends IGenericRepository<OrdemServicoModel> {
-    Long gerarProximoNumero();
+    List<OrdemServicoModel> findAllByIdVeiculo(Integer idVeiculo);
+    List<OrdemServicoModel> findAllByStatus(String status);
 }
