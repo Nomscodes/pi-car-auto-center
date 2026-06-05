0c185e2ad399c92a250a8b40935b2daff76b2e0c
Caio4breu <abreuocaio@gmail.com>
2026-06-05 02:47:16 -0300

    refactor(veiculo): corrige model, dto, repository e validation para o schema real

--- src/main/java/br/com/picarauto/model/VeiculoModel.java
+++ src/main/java/br/com/picarauto/model/VeiculoModel.java
@@ -1,26 +1,24 @@
 package br.com.picarauto.model;
 
+/**
+ *
+ * @author Caio4breu
+ */
 public class VeiculoModel extends BaseModel {
     private String placa;
-    private String marca;
-    private String modelo;
-    private Integer anoFabricacao;
     private String cor;
-    private Integer quilometragem;
-    private ClienteModel cliente;
+    private String chassi;
+    private Integer idModelo;
+    private Integer idCliente;
 
     public String getPlaca() { return placa; }
     public void setPlaca(String placa) { this.placa = placa; }
-    public String getMarca() { return marca; }
-    public void setMarca(String marca) { this.marca = marca; }
-    public String getModelo() { return modelo; }
-    public void setModelo(String modelo) { this.modelo = modelo; }
-    public Integer getAnoFabricacao() { return anoFabricacao; }
-    public void setAnoFabricacao(Integer anoFabricacao) { this.anoFabricacao = anoFabricacao; }
     public String getCor() { return cor; }
     public void setCor(String cor) { this.cor = cor; }
-    public Integer getQuilometragem() { return quilometragem; }
-    public void setQuilometragem(Integer quilometragem) { this.quilometragem = quilometragem; }
-    public ClienteModel getCliente() { return cliente; }
-    public void setCliente(ClienteModel cliente) { this.cliente = cliente; }
+    public String getChassi() { return chassi; }
+    public void setChassi(String chassi) { this.chassi = chassi; }
+    public Integer getIdModelo() { return idModelo; }
+    public void setIdModelo(Integer idModelo) { this.idModelo = idModelo; }
+    public Integer getIdCliente() { return idCliente; }
+    public void setIdCliente(Integer idCliente) { this.idCliente = idCliente; }
 }
