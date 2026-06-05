5731ceeba2c296217d08de46d18341faa410637c
Caio4breu <abreuocaio@gmail.com>
2026-06-05 19:40:40 -0300

    feat(os): adiciona IMarcaValidation e MarcaValidation com capitalizacao automatica

---
+++ src/main/java/br/com/picarauto/validation/IMarcaValidation.java
@@ -0,0 +1,10 @@
+package br.com.picarauto.validation;
+
+import br.com.picarauto.model.MarcaModel;
+import br.com.picarauto.repository.IMarcaRepository;
+
+/**
+ *
+ * @author Caio4breu
+ */
+public interface IMarcaValidation extends IGenericValidation<MarcaModel, IMarcaRepository> {}
