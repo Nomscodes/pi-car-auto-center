09a2b2f06036d09c9914c65b1ba1b26894a955fa
Caio4breu <abreuocaio@gmail.com>
2026-06-05 03:50:33 -0300

    refactor(colaborador): remove camada Mecanico substituída por ColaboradorModel

--- src/main/java/br/com/picarauto/service/IMecanicoService.java
+++
@@ -1,7 +0,0 @@
-package br.com.picarauto.service;
-
-import br.com.picarauto.model.MecanicoModel;
-import br.com.picarauto.repository.IMecanicoRepository;
-import br.com.picarauto.validation.IMecanicoValidation;
-
-public interface IMecanicoService extends IGenericService<MecanicoModel, IMecanicoRepository, IMecanicoValidation> {}
