79dbfebae4b169d708798a0c29907b32fb5d2f93
Caio4breu <abreuocaio@gmail.com>
2026-06-05 19:34:25 -0300

    refactor(util): remove default methods de IGenericValidation e ajusta GenericValidation

--- src/main/java/br/com/picarauto/validation/IGenericValidation.java
+++ src/main/java/br/com/picarauto/validation/IGenericValidation.java
@@ -3,16 +3,15 @@ package br.com.picarauto.validation;
 import br.com.picarauto.model.BaseModel;
 import br.com.picarauto.repository.IGenericRepository;
 
+/**
+ * 
+ * @author Caio4breu
+ */
 public interface IGenericValidation<E extends BaseModel, R extends IGenericRepository<E>> {
     void validateFields(E entity);
-
     default void validateFieldsInsert(E entity) {}
-
     default void validateFieldsUpdate(E entity) {}
-
     default void validateInsert(E entity) {}
-
     default void validateUpdate(E entity) {}
-
     default void validateDelete(Integer id) {}
 }
