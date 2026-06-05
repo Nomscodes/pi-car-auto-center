a9c34d60a57e67543cd0fd7539b88c2f5b600746
Caio4breu <abreuocaio@gmail.com>
2026-06-05 15:34:32 -0300

    feat(colaborador): adiciona model, DTO e repository de FuncaoColaborador

---
+++ src/main/java/br/com/picarauto/model/dto/FuncaoColaboradorDTO.java
@@ -0,0 +1,13 @@
+package br.com.picarauto.model.dto;
+
+/**
+ *
+ * @author Caio4breu
+ */
+public class FuncaoColaboradorDTO extends BaseDTO {
+
+    private String funcao;
+
+    public String getFuncao() { return funcao; }
+    public void setFuncao(String funcao) { this.funcao = funcao; }
+}
