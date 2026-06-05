0e7b334231602db728a9e9e97633798fac4ff254
Caio4breu <abreuocaio@gmail.com>
2026-06-05 03:24:31 -0300

    refactor(os): remove camada de mappers do pacote controller

--- src/main/java/br/com/picarauto/controller/mapper/IClienteMapper.java
+++
@@ -1,6 +0,0 @@
-package br.com.picarauto.controller.mapper;
-
-import br.com.picarauto.model.ClienteModel;
-import br.com.picarauto.model.dto.ClienteDTO;
-
-public interface IClienteMapper extends IGenericMapper<ClienteModel, ClienteDTO> {}
--- src/main/java/br/com/picarauto/controller/mapper/IOrdemServicoMapper.java
+++
@@ -1,6 +0,0 @@
-package br.com.picarauto.controller.mapper;
-
-import br.com.picarauto.model.OrdemServicoModel;
-import br.com.picarauto.model.dto.OrdemServicoDTO;
-
-public interface IOrdemServicoMapper extends IGenericMapper<OrdemServicoModel, OrdemServicoDTO> {}
