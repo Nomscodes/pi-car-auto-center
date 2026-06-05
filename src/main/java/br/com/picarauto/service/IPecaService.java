4a1d52ffb55c948dfb3c5383b39054ca4fab0ed0
Caio4breu <abreuocaio@gmail.com>
2026-06-05 02:46:55 -0300

    refactor(peca): reescreve cadeia completa para o schema real

--- src/main/java/br/com/picarauto/service/IPecaService.java
+++ src/main/java/br/com/picarauto/service/IPecaService.java
@@ -4,4 +4,11 @@ import br.com.picarauto.model.PecaModel;
 import br.com.picarauto.repository.IPecaRepository;
 import br.com.picarauto.validation.IPecaValidation;
 
-public interface IPecaService extends IGenericService<PecaModel, IPecaRepository, IPecaValidation> {}
+/**
+ *
+ * @author Caio4breu
+ */
+public interface IPecaService extends IGenericService<PecaModel, IPecaRepository, IPecaValidation> {
+
+    PecaModel findByCodigoNacional(Integer codigoNacional);
+}
