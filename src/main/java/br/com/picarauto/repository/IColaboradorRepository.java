88a3167b79668d871151b7fe8bf99c99d20c400f
Gabriel Naoki <gabrielnaokiu@gmail.com>
2026-06-07 12:34:13 -0300

    refactor(colaborador): adiciona existsByEmail em IColaboradorRepository

--- src/main/java/br/com/picarauto/repository/IColaboradorRepository.java
+++ src/main/java/br/com/picarauto/repository/IColaboradorRepository.java
@@ -9,4 +9,6 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface IColaboradorRepository extends IGenericRepository<ColaboradorModel> {
+
+    boolean existsByEmaul(String email);
 }
