c2766cefe0079afcc7243000c6df7973fccc4806
Caio4breu <abreuocaio@gmail.com>
2026-06-08 04:26:26 -0300

    refactor(db): adiciona anotacoes JPA aos models de chave composta

--- src/main/java/br/com/picarauto/model/ItemFornecedorModel.java
+++ src/main/java/br/com/picarauto/model/ItemFornecedorModel.java
@@ -1,43 +1,70 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package br.com.picarauto.model;
 
-import br.com.picarauto.model.base.BaseModel;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import java.time.LocalDate;
 
 /**
+ * Representa o vínculo entre um fornecedor e o serviço externo que ele prestou,
+ * registrando a data de execução.
+ *
+ * Tabela com chave composta (idFornecedor + idItemPedidoServicoExterno + dataExecucao).
+ * Não estende BaseModel pois não possui PK serial, ativo nem data_hora_criacao.
+ * O acesso é feito exclusivamente via
+ * {@link br.com.picarauto.repository.ItemFornecedorRepository}
+ * usando EntityManager com queries nativas.
  *
  * @author Gabriel
  */
-public class ItemFornecedorModel extends BaseModel {
-    
-    private Integer idFornecedor;
-    private Integer idItemPedidoServicoExterno;
+@Data
+@EqualsAndHashCode
+@Entity
+@Table(name = "itemFornecedor")
+@IdClass(ItemFornecedorModel.ChaveComposta.class)
+public class ItemFornecedorModel {
+
+    @Id
+    @Column(name = "idFornecedor", nullable = false)
+    private Long idFornecedor;
+
+    @Id
+    @Column(name = "idItemPedidoServicoExterno", nullable = false)
+    private Long idItemPedidoServicoExterno;
+
+    @Id
+    @Column(name = "dataExecucao", nullable = false)
     private LocalDate dataExecucao;
 
-    public Integer getIdFornecedor() {
-        return idFornecedor;
-    }
+    /**
+     * Classe auxiliar que representa a chave composta.
+     * Necessária para o JPA identificar registros unicamente.
+     */
+    public static class ChaveComposta implements java.io.Serializable {
+        private Long idFornecedor;
+        private Long idItemPedidoServicoExterno;
+        private LocalDate dataExecucao;
 
-    public void setIdFornecedor(Integer idFornecedor) {
-        this.idFornecedor = idFornecedor;
-    }
+        public ChaveComposta() {}
 
-    public Integer getIdItemPedidoServicoExterno() {
-        return idItemPedidoServicoExterno;
-    }
+        public ChaveComposta(Long idFornecedor, Long idItemPedidoServicoExterno, LocalDate dataExecucao) {
+            this.idFornecedor                  = idFornecedor;
+            this.idItemPedidoServicoExterno    = idItemPedidoServicoExterno;
+            this.dataExecucao                  = dataExecucao;
+        }
 
-    public void setIdItemPedidoServicoExterno(Integer idItemPedidoServicoExterno) {
-        this.idItemPedidoServicoExterno = idItemPedidoServicoExterno;
-    }
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof ChaveComposta that)) return false;
+            return java.util.Objects.equals(idFornecedor, that.idFornecedor)
+                && java.util.Objects.equals(idItemPedidoServicoExterno, that.idItemPedidoServicoExterno)
+                && java.util.Objects.equals(dataExecucao, that.dataExecucao);
+        }
 
-    public LocalDate getDataExecucao() {
-        return dataExecucao;
-    }
-
-    public void setDataExecucao(LocalDate dataExecucao) {
-        this.dataExecucao = dataExecucao;
+        @Override
+        public int hashCode() {
+            return java.util.Objects.hash(idFornecedor, idItemPedidoServicoExterno, dataExecucao);
+        }
     }
 }
