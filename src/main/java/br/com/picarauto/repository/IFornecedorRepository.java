d768c73c78bf2c51c588d42bfc2330e62230279e
Caio4breu <abreuocaio@gmail.com>
2026-06-08 04:40:46 -0300

    fix(repositories): corrige variaveis utilizadas no banco de dados

--- src/main/java/br/com/picarauto/repository/IFornecedorRepository.java
+++ src/main/java/br/com/picarauto/repository/IFornecedorRepository.java
@@ -1,19 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package br.com.picarauto.repository;
 
 import br.com.picarauto.model.FornecedorModel;
 
 /**
- *
+ * 
  * @author Gabriel
  */
-@Repository
 public interface IFornecedorRepository extends IGenericRepository<FornecedorModel> {
-
     boolean existsByCnpj(String cnpj);
-
     boolean existsByTelefone(String telefone);
 }
