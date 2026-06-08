7bcff5045bc4c1890b226ed66385595b5701e9af
Gabriel Naoki <gabrielnaokiu@gmail.com>
2026-06-08 16:58:16 -0300

    feat(fornecedor): adiciona IFornecedorValidation e FornecedorValidation

---
+++ src/main/java/br/com/picarauto/validation/IFornecedorValidation.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.com.picarauto.validation;
+
+import br.com.picarauto.model.FornecedorModel;
+import br.com.picarauto.repository.IFornecedorRepository;
+
+/**
+ *
+ * @author Gabriel
+ */
+public interface IFornecedorValidation extends IGenericValidation<FornecedorModel, IFornecedorRepository> {
+    
+}
