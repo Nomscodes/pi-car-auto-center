f0a5c0faa05c1076f1c289881d3e86c37f52cf31
Gabriel Naoki <gabrielnaokiu@gmail.com>
2026-06-11 15:46:07 -0300

    feat(os): implementa services de ServicoExterno e ServicoInterno

---
+++ src/main/java/br/com/picarauto/service/ServicoInternoService.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.picarauto.service;
+
+import br.com.picarauto.model.ServicoInternoModel;
+import br.com.picarauto.repository.IServicoInternoRepository;
+import br.com.picarauto.validation.IServicoInternoValidation;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Gabriel
+ */
+
+@Service
+public class ServicoInternoService extends GenericService<ServicoInternoModel, IServicoInternoRepository, IServicoInternoValidation> implements IServicoInternoService{
+    
+    public ServicoInternoService(IServicoInternoRepository repository, IServicoInternoValidation validation) {
+        super(repository, validation);
+    }
+}
