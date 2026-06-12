b971695e686eebd4452c47c11eaf4039caf478ae
Gabriel Naoki Uto Turigoe <gabrielnaokiu@gmail.com>
2026-06-11 21:05:45 -0300

    Revert "feat(os): implementa controllers de ServicoExterno e ServicoInterno"

--- src/main/java/br/com/picarauto/repository/IUsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.picarauto.repository;
-
-import br.com.picarauto.model.UsuarioModel;
-
-public interface IUsuarioRepository extends IGenericRepository<UsuarioModel> {
-    boolean existsByLogin(String login);
-    UsuarioModel findByLogin(String login);
-}
