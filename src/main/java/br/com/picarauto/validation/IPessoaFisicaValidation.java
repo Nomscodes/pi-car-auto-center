c00623ab7877166c35d16afa1f913feef3a5f4c4
Caio4breu <abreuocaio@gmail.com>
2026-06-13 03:33:35 -0300

    feat(cliente): adiciona stack completa de Pessoa Física (controller, service, validation)

---
+++ src/main/java/br/com/picarauto/validation/IPessoaFisicaValidation.java
@@ -0,0 +1,10 @@
+package br.com.picarauto.validation;
+
+import br.com.picarauto.model.PessoaFisicaModel;
+import br.com.picarauto.repository.IPessoaFisicaRepository;
+
+/**
+ *
+ * @author Caio4breu
+ */
+public interface IPessoaFisicaValidation extends IGenericValidation<PessoaFisicaModel, IPessoaFisicaRepository> {}
