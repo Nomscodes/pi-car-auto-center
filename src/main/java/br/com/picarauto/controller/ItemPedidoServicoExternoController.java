80df0300408a0a466ed3c4769693d39fdee1390f
Caio4breu <abreuocaio@gmail.com>
2026-06-15 14:13:17 -0300

    fix(os): corrige factory method nos controllers de itens de servico

--- src/main/java/br/com/picarauto/controller/ItemPedidoServicoExternoController.java
+++ src/main/java/br/com/picarauto/controller/ItemPedidoServicoExternoController.java
@@ -1,9 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package br.com.picarauto.controller;
 
+import br.com.picarauto.factory.IServicoItemFactory;
+import br.com.picarauto.factory.ServicoExternoFactory;
 import br.com.picarauto.model.ItemPedidoServicoExternoModel;
 import br.com.picarauto.service.IItemPedidoServicoExternoService;
 import org.springframework.stereotype.Component;
@@ -17,11 +15,18 @@ import java.util.List;
 @Component
 public class ItemPedidoServicoExternoController extends GenericController<ItemPedidoServicoExternoModel, IItemPedidoServicoExternoService> {
 
+    // Padrão de Projeto: Factory Method
+    private final IServicoItemFactory itemFactory = new ServicoExternoFactory();
+
     public ItemPedidoServicoExternoController(IItemPedidoServicoExternoService service) {
         super(service);
     }
 
     public List<ItemPedidoServicoExternoModel> findAllByIdOS(Long idOS) {
         return service.findAllByIdOS(idOS);
+    }
+
+    public ItemPedidoServicoExternoModel novoItem() {
+        return (ItemPedidoServicoExternoModel) itemFactory.criar();
     }
 }
